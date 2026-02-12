24af4c69c9d8c551a869a3be3fc9dda411a4f244
ptrbortolotti <ptrbortolotti@gmail.com>
2026-02-12 14:48:07 -0700

    let's turn wombat off for the 3.4mw, which is used in some tests within an iterative optimization

--- wisdem/test/test_gluecode/test_gluecode.py
+++ wisdem/test/test_gluecode/test_gluecode.py
@@ -49,7 +49,7 @@ class TestRegression(unittest.TestCase):
 
         self.assertAlmostEqual(wt_opt["rotorse.rp.AEP"][0] * 1.0e-6, 13.591140338759166, 1)
         self.assertAlmostEqual(wt_opt["rotorse.blade_mass"][0], 14534.711602944584, 1)  # new value: improved interpolation
-        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 37.355115146426364, 1)
+        self.assertAlmostEqual(wt_opt["financese.lcoe"][0] * 1.0e3, 38.444833824949285, 1)
         self.assertAlmostEqual(wt_opt["rotorse.rs.tip_pos.tip_deflection"][0], 8.031667548036724, 1)
         self.assertAlmostEqual(wt_opt["towerse.z_param"][-1], 108.0, 3)
 
